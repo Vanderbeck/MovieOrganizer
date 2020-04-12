c5fc1da2c85f1317c86bbd745ec9f8ec84d581e3
Lindsay Vanderbeck <lvanderbeck@macleanengieering.com>
2020-04-12 11:29:26 -0400

    List data in test folder.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #! /usr/bin/python3
-print("Hello World")
+import os
+
+
+
+
+def main():
+    input_folder='/home/lindsay/Projects/MovieOrganizer/testData/input'
+    output_folder='/home/lindsay/Projects/MovieOrganizer/testData/output'
+
+    # List everything in the working folder
+    folder_contents= os.system('ls '+input_folder)
+    print(folder_contents)
+
+
+if __name__=="__main__":
+    main()
