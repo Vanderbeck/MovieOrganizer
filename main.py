766af76e951b0362212c891d4e7042f3880f7e90
Lindsay Vanderbeck <lvanderbeck@macleanengieering.com>
2020-04-12 21:42:52 -0400

    Wroking rename code for movies. No file moving yet.

--- main.py
+++ main.py
@@ -34,11 +34,12 @@ class MovieOrganizer():
     def handleFiles(self):
         while self.files:
             self.filename = self.files.pop()
-            print("File to change: ", self.filename)
+            self.splitNameAndExtension()
+            print("File to change: ", self.new_filename)
             loop=True
             while loop:
                 # Prompt for input: Movie or Show
-                tmp = input('\n\nIs it a Movie (1) or a TV Show (2)?\nPress 0 to skip\n')
+                tmp = input('\n\nIs it a Movie (1) or a TV Show (2)? Press (0) to skip\n')
                 if not self.correctInput(tmp, 2):
                     continue
                 input_int = int(tmp)
@@ -52,20 +53,28 @@ class MovieOrganizer():
                 elif input_int==2:
                     loop = self.show()
                     print("\nTV Show\n")
+                # Prompt user to change filename and move OR restart
+                if loop:
+                    continue #Ignore if the other methods failed
+                #Prompt
+                #restart?
+                #copy to output_folder?
 
 
-
-    def splitNameAndExtension(self, file):
+    def splitNameAndExtension(self):
+        print(self.filename)
         idx = len(self.filename) #file is the filename string
+        print("idx Prior ", idx)
         while True:
             idx-=1
             tmp = self.filename[idx]
-            if tmp!=".":
+            if tmp!='.':
                 continue
             else:
                 # Takes the extension including the period and stores it
                 self.ext=self.filename[idx:len(self.filename)]
-                self.filename=self.filename[0:idx]
+                self.new_filename=self.filename[0:idx]
+                break
 
 
     def correctInput(self, input, max):
@@ -84,20 +93,25 @@ class MovieOrganizer():
         return out
 
     def movie(self):
-        outer_loop = True # The return value to break the loop in handleFiles()
+        outer_loop_continue=True
         while True:
             TD_out=self.titleDelimiter()
             if TD_out:
                 break
         while True:
-            NL_out=self.newLength()
-            if NL_out:
+            TR_out=self.trimRight()
+            if TR_out:
+                break
+        while True:
+            TL_out=self.trimLeft()
+            if TL_out:
                 break
         while True:
             YB_out=self.yearBraket()
             if YB_out:
                 break
-        return outer_loop
+        outer_loop_continue=False
+        return outer_loop_continue # Return False to break the outer loop in handleFiles()
 
     def show(self):
         pass
@@ -105,7 +119,7 @@ class MovieOrganizer():
     def titleDelimiter(self):
         out=False
         # Request Input from User
-        delim=input('File: '+ self.filename +
+        delim=input('Filename so far: '+ self.filename +
                     '\nSplit with delimeter --> ')
         # Sanitize delimeter input
         try:
@@ -124,23 +138,106 @@ class MovieOrganizer():
             filename_split = self.filename
         # Show resultant title and confirm with user
         print("New Filename: ", filename_split)
-        user_input=input("Was the title broken up correctly? (Y/n) --> ")
-        if user_input == 'n' or user_input=='N':
-            out = False
-        else:
+        user_input=input("Was the title broken up correctly? (y/N) --> ")
+        if user_input == 'y' or user_input=='Y':
             self.new_filename = filename_split
             print(self.new_filename)
             out = True
+        else:
+            out = False
+
             # os.system("mkdir -p "+self.output_folder+"/"+file_folder)
         # Return
         return out
 
+    def trimRight(self):
+        out=False
+        # Request Input from User
+        nTrim=input('Filename so far: '+ self.new_filename +
+                    '\nTrim the right by how many chars? (guess) --> ')
+        # Sanitize trim number input
+        try:
+            nTrim=int(nTrim)
+        except:
+            print("Not a number")
+            return out
+        #Check if input was a proper trim value
+        if nTrim <0 or nTrim>=len(self.new_filename):
+            print("Try another trim input. Too big? Too small?")
+            return out
+        # Shorten the filename
+        filename_short = str(self.new_filename[0:len(self.new_filename)-nTrim])
+        # Display filename to the User and confirm
+        print("New Filename: ", filename_short)
+        user_input=input("Was the title shortened correctly? (y/N) --> ")
+        if user_input == 'y' or user_input=='Y':
+            self.new_filename = filename_short
+            print(self.new_filename)
+            out = True
+        else:
+            out = False
+            # os.system("mkdir -p "+self.output_folder+"/"+file_folder)
+        # Return
+        return out
 
-
-    def newLength(self):
-        pass
+    def trimLeft(self):
+        out=False
+        # Request Input from User
+        nTrim=input('Filename so far: '+ self.new_filename +
+                    '\nTrim the right by how many chars? (guess) --> ')
+        # Sanitize trim number input
+        try:
+            nTrim=int(nTrim)
+        except:
+            print("Not a number")
+            return out
+        #Check if input was a proper trim value
+        if nTrim <0 or nTrim>=len(self.new_filename):
+            print("Try another trim input. Too big? Too small?")
+            return out
+        # Shorten the filename
+        filename_short = str(self.new_filename[nTrim:len(self.new_filename)])
+        # Display filename to the User and confirm
+        print("New Filename: ", filename_short)
+        user_input=input("Was the title shortened correctly? (y/N) --> ")
+        if user_input == 'y' or user_input=='Y':
+            self.new_filename = filename_short
+            print(self.new_filename)
+            out = True
+        else:
+            out = False
+        # Return
+        return out
 
     def yearBraket(self):
+        out=False
+        # Request Input from User
+        year=input('Filename so far: '+ self.new_filename +
+                    '\nAdd year to the end of title (hit enter for no year) --> ')
+        # Sanitize trim number input
+        try:
+            year=str(year)
+        except:
+            print("Input Error")
+            return out
+        # Add year to filename
+        if year != '':
+            filename_year = self.new_filename + ' ('+year+')'
+        else:
+            filename_year = self.new_filename
+        # Display filename to the User and confirm
+        print("New Filename: ", filename_year)
+        user_input=input("Was the title shortened correctly? (y/N) --> ")
+        if user_input == 'y' or user_input=='Y':
+            self.new_filename = filename_year
+            print(self.new_filename)
+            out = True
+        else:
+            out = False
+        # Return
+        return out
+
+    def episodeNumber(self):
         pass
 
 
